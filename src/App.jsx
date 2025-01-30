d32c7b309d953590345bd70d38df84eca403277f
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-01-30 20:26:06 +0530

    chore: add 'contact me' section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FloatingBar from './components/FloatingBar';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import ContactMe from './components/ContactMe';
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
       <Header />
       <About />
       <Projects />
+      <ContactMe />
     </Background>
     </>
   );
