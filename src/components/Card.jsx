7d8ae3b32248d0be7c752dad3e4d34bea6d2e06b
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-01-30 19:23:21 +0530

    chore: improve

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Card = ({ title, time, description, frameworks, link }) => {
   return (
     <div className="rounded-lg bg-card text-card-foreground flex flex-col overflow-hidden border hover:shadow-lg min-w-64">
-      <div className="aspect-video w-full overflow-hidden bg-stone-700"></div>
+      <div className="aspect-video w-full overflow-hidden">
+      <video 
+  muted 
+  autoPlay 
+  loop 
+  className="w-full h-full"
+  poster="/thumbnail/skycast.jpg"
+>
+  <source src="/videos/skycast.mp4" type="video/mp4" />
+</video>
+
+      </div>
       <div className=" flex flex-col p-3">
         <div className="font-bold mt-1 text-lg">{title}</div>
         <div className="font-sans text-sm opacity-80">{time}</div>
@@ -11,8 +22,8 @@ const Card = ({ title, time, description, frameworks, link }) => {
           {description}
         </div>
         <div className="flex flex-wrap mt-2">
-          {frameworks.map((framework) => (
-            <div className="bg-secondary text-secondary-foreground font-semibold rounded-md px-2 py-1 text-xs mr-2 mb-2">
+          {frameworks.map((index,framework) => (
+            <div className="bg-secondary text-secondary-foreground font-semibold rounded-md px-2 py-1 text-xs mr-2 mb-2" key={index}>
               {framework}
             </div>
           ))}
