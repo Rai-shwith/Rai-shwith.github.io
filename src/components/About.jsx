b4c18c072c62b285e9ee46659494babd069484f5
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-02-01 21:08:10 +0530

    chore: add Bangalore , fix #2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import Description from "./Description";
 
 const About = () => {
   return (
     <div className="flex flex-col my-8">
       <h3 className="font-bold text-xl">About</h3>
-      <p className="text-muted-foreground">
-        I'm a passionate <span className="text-muted-foreground font-bold">software developer</span> who loves turning ideas into functional,
-        user-friendly applications. From crafting sleek <span className="text-muted-foreground font-bold">weather apps</span> to
-        automating <span className="text-muted-foreground font-bold">LeetCode-to-GitHub</span> workflows, I enjoy building tools that
-        make life easier. Recently, I developed <span className="text-muted-foreground font-bold">Leet2Git</span>, a tool that transforms
+      <Description
+        description={`I'm a passionate *software developer* from Bangalore who loves turning ideas into functional,
+        user-friendly applications. From crafting sleek *weather apps* to
+        automating *LeetCode-to-GitHub* workflows, I enjoy building tools that
+        make life easier. Recently, I developed *Leet2Git*, a tool that transforms
         LeetCode problems into well-structured GitHub repositories, and
-        <span className="text-muted-foreground font-bold">Get-My-Attendance</span>, a WiFi-based attendance system. I also won the <span className="text-muted-foreground font-bold">Best
-        About Us Page Award</span> in a web design competition and ranked <span className="text-muted-foreground font-bold">Top 5 in a
-        college hackathon</span>. Currently, I'm sharpening my web development skills
-        while gearing up to explore AI. 🚀
-      </p>
+        *Get-My-Attendance*, a WiFi-based attendance system. I also won the *Best
+        About Us Page Award* in a web design competition and ranked *Top 5 in a
+        college hackathon*. Currently, I'm sharpening my web development skills
+        while gearing up to explore AI. 🚀`}
+        normalClassNames={"text-muted-foreground "}
+        specialClassName={"text-muted-foreground font-bold"}
+      />
     </div>
   );
 };
